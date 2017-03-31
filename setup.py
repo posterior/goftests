8eca594e791678fc1ad38fbaebd30b50b59f1537
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2017-03-31 00:05:24 -0400

    Moves scipy from install to test requirements

--- setup.py
+++ setup.py
@@ -59,9 +59,10 @@ config = {
     'maintainer': 'Fritz Obermeyer',
     'maintainer_email': 'fritz.obermeyer@gmail.com',
     'license': 'Revised BSD',
-    'install_requires': ['kdtree', 'numpy', 'scipy'],
+    'install_requires': ['kdtree', 'numpy'],
     'packages': ['goftests'],
     'test_suite': 'goftests.test',
+    'tests_require': ['scipy'],
     'include_package_data': True,
 }
 
